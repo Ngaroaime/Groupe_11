171b56f09d59c7e289aca51c81469b075ddd7e93
Ngaroaime <56893100+Ngaroaime@users.noreply.github.com>
2019-11-10 12:07:18 +0100

    Create Contrôleur.py

---
+++ Contrôleur.py
@@ -0,0 +1,17 @@
+. # Ngaroaime
+. #Matricule: 17A936FS
+.
+.frompersonneimportpersonne
+.
+classcontroleur (personne)
+.def_init_ (auto):
+. personne_init_ (self);
+Un séjour sans faille
+. Défaillant (solde, mt):
+.si (solde <mt);
+.retour0; #pour false
+.else:
+.retour1; #verstrue
+
+.def_str_ (auto):
+.revenir "\ tcontroleur \ n" + personne_str_ (self).
